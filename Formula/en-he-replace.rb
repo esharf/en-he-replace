8d0ccee46580d926feb0e0b530af320d87dc8afe
esharf <runner@sjc20-cw715-13de276c-04c3-434d-a12e-ccc7f5f25415-EA5CF1AB2B78.local>
2025-12-15 10:07:49 +0000

    Update formula file to version 0.0.5

--- Formula/en-he-replace.rb
+++ Formula/en-he-replace.rb
@@ -1,9 +1,9 @@
 class AwsUrlCopyTool < Formula
   desc "Auto-modify macOS clipboard for AWS URLs"
   homepage "https://github.com/esharf/en-he-replace"
-  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.4/en-he-replace-macos.zip"
-  sha256 "69314efb1e17b712eadd5c81596410fe82ad4b1b7da09dfcd6f664496585b72c"
-  version "0.0.4"
+  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.5/en-he-replace-macos.zip"
+  sha256 "250c0ab5c57d38507fbd5e92b3d90537cd1bdff2f7f9287ddd6a925d1d6a205d"
+  version "0.0.5"
 
   def install
     bin.install "en-he-replace"

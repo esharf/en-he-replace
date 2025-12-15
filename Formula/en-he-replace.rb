1e7bf6f4ae0e33b5a7425de37e354cbaed72a93f
esharf <runner@sat12-dp147-0ac3f59c-e490-4708-bc5a-7e23982a6d85-3EF5AE9E8E6D.local>
2025-12-15 09:21:27 +0000

    Update formula file to version 0.0.2

--- Formula/en-he-replace.rb
+++ Formula/en-he-replace.rb
@@ -1,11 +1,22 @@
 class AwsUrlCopyTool < Formula
   desc "Auto-modify macOS clipboard for AWS URLs"
   homepage "https://github.com/esharf/en-he-replace"
-  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.1/en-he-replace-macos.zip"
-  sha256 "207d6d7f5db2d8099c9f88e4271b028903b8520a0327af82128a7b5b8f96ebc5"
-  version "0.0.1"
+  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.2/en-he-replace-macos.zip"
+  sha256 "0f57073397d7288101cfc8b4502a2a8ec25121e3ce49cdfd3986996e3ee8b0d0"
+  version "0.0.2"
 
   def install
     bin.install "en-he-replace"
+    pkgshare.install "en<->he.workflow"
+  end
+
+  def caveats
+    <<~EOS
+      To enable the included Automator service, copy the workflow to your Services folder:
+
+        cp -R "#{pkgshare}/en<->he.workflow" "~/Library/Services/"
+
+      After copying, the service will be available in the Services menu.
+    EOS
   end
 end
